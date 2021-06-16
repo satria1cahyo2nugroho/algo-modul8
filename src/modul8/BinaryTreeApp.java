ca9d53404cb9474fd0c0b34f8509f6b4b07db854
User <User@DESKTOP-TRC66AO>
2021-06-16 21:36:46 +0700

    tugas modul 8 algoritma struktur 2 kelasd C

---
+++ src/modul8/BinaryTreeApp.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul8;
+
+/**
+ *
+ * @author User
+ */
+public class BinaryTreeApp {
+  public static void main(String[] args) {
+        BinaryTree tree=new BinaryTree();
+        
+        TreeNode node;
+        
+        node = new TreeNode('D');
+        tree.insert(node);
+        
+        node = new TreeNode('B');
+        tree.insert(node);
+        
+        node = new TreeNode('A');
+        tree.insert(node);
+        
+        node = new TreeNode('C');
+        tree.insert(node);
+        
+        node = new TreeNode('F');
+        tree.insert(node);
+        
+        node = new TreeNode('E');
+        tree.insert(node);
+        
+        node = new TreeNode('G');
+        tree.insert(node);
+        
+        System.out.print("Traversal dengan preorder :");
+        tree.preOrder();
+        System.out.print("\nTraversal dengan inorder :");
+        tree.inOrder();
+        System.out.print("\nTraversal dengan postorder :");
+        tree.postOrder();
+        System.out.println();
+    }
+}
